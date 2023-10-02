480b3e5dbfbfce6fd4c8a6d043913bdf5298d9e7
vcc64 <39348910+vcc64@users.noreply.github.com>
2023-10-02 18:31:44 +0100

    correct middle point for even length

--- median.py
+++ median.py
@@ -15,7 +15,7 @@ while True:
 numbers = sorted(numbers)
 
 if len(numbers)%2 == 0:
-    middlePoint = int(len(numbers)/2)
+    middlePoint = int(len(numbers)/2) - 1
     median = (numbers[middlePoint] + numbers[middlePoint+1])/2
 else:
     middlePoint = math.floor(len(numbers)/2)
